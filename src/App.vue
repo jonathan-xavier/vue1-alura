03903f259a0f663d5fae895c5187bb7352f927a4
jonathan xavier <jonathan.rodrigues@mv.com.br>
2025-06-10 14:08:35 -0300

    criando componente principal

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <Banner />    
+  <Banner />  
+  <ConteudoPrincipal/>  
 </template>
 
 
 <script lang="ts">
 import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
 
 export default {
   components: {
     Banner,
+    ConteudoPrincipal,
   }
 }
 </script>
