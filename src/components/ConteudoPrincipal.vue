66d7327742ed6f97b9f5e2760d87b526a968e7d9
jonathan <jonathanxavier556@gmail.com>
2025-06-11 00:28:15 -0300

    adicionando v-if e v-else

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -4,20 +4,25 @@
             <span class="subtitulo-lg sua-lista-texto">
                 Sua lista:
             </span>
-            <ul class="ingredientes-sua-lista">
-                <li v-for="ingrediente in ingredientes" class="ingrediente">
+            <ul v-if="ingredientes" class="ingredientes-sua-lista">
+                <li v-for="ingrediente in ingredientes" 
+                :key="ingrediente" class="ingrediente">
                     {{ ingrediente }}
                 </li>
-                
             </ul>
+            <p  v-else class="paragrafo lista-vazia">
+              <img src="../assets/images/icones/lista-vazia.svg" alt="lista vazia">
+              Sua lista está vazia. Selecione ingredientes para iniciar.
+            </p>
         </section>
     </main>
 </template>
 
-<script>
+<script lang="ts">
 export default {
     data() {
         return {
+          
             ingredientes: ['Alho', 'Manteiga', 'Orégano']
         }
     }
